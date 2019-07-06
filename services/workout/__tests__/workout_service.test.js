63c2ad9a1c98a008e99c84f8206c3a279b9b79c9
Anton <anton.moroz@helsinki.fi>
2019-07-06 21:03:31 +0300

    Another test on creating a workout. Skipped stub tests for UPDATE/DELETE added as well

--- services/workout/__tests__/workout_service.test.js
+++ services/workout/__tests__/workout_service.test.js
@@ -19,3 +19,39 @@ describe('listWorkouts test', () => {
     expect(MockWorkoutModel.find.callCount).toBe(1)
   })
 })
+
+describe('createWorkout test', () => {
+  it('succesfully creates a workout with valid data', () => {
+    const save = sinon.spy()
+    let content
+
+    // Arrow function does not work here?
+    const MockWorkoutModel = function (data) {
+      content = data.content
+      // console.log('given content', content)
+      return {
+        ...data,
+        save
+      }
+    }
+
+    const workoutService = WorkoutService(MockWorkoutModel)
+
+    // parameter works as data for MockWorkoutModel
+    workoutService.createWorkout('overhead press 5x55kg')
+
+    // NOTICE DIFFERENCE COMPARED TO listWorkouts!
+    expect(save.callCount).toBe(1)
+    expect(content).toEqual('overhead press 5x55kg')
+  })
+})
+
+describe('updateWorkout test', () => {
+  it.skip('updates a workout with valid id', () => {
+  })
+})
+
+describe('deleteWorkout test', () => {
+  it.skip('deletes a workout with valid id', () => {
+  })
+})
--- services/workout/workout_service.js
+++ services/workout/workout_service.js
@@ -2,8 +2,17 @@ const listWorkouts = Workout => () => {
   return Workout.find({})
 }
 
+const createWorkout = Workout => (content) => {
+  if (!content)
+    throw new Error(`Content: ${content}`)
+
+  const session = new Workout({ content })
+  return session.save()
+}
+
 module.exports = Workout => {
   return {
-    listWorkouts: listWorkouts(Workout)
+    listWorkouts: listWorkouts(Workout),
+    createWorkout: createWorkout(Workout)
   }
 }

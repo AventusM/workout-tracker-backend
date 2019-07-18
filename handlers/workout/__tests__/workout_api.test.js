43d19ae243e039858adf0b9221a92ed0c35c4849
Anton <anton.moroz@helsinki.fi>
2019-07-18 18:06:51 +0300

    Improvements with test configurations and learning little more about sinon

--- handlers/workout/__tests__/workout_api.test.js
+++ handlers/workout/__tests__/workout_api.test.js
@@ -1,5 +1,6 @@
 const test = require('ava')
-const sinon = require('sinon')
+// allows for quick sinon.restore() instead of restoring other things 1 by 1
+const sinon = require('sinon').sandbox.create()
 const { getAllWorkouts } = require('../index')
 const WorkoutService = require('../../../services/workout/index')
 
@@ -10,20 +11,25 @@ const mockReq = (sessionData, body) => ({
 
 const mockRes = () => {
   const res = {}
+  // Stubbing like this allows for chaining res.status(NUMBER).json(OBJECT) ...
   res.status = sinon.stub().returns(res)
   res.json = sinon.stub().returns(res)
-  return res;
+  return res
 }
 
 const mockNext = () => { }
 
 test.beforeEach(() => {
+  // Currently this (integration) test doesn't care for what this promise resolves into
+  // Only the status code
   sinon.stub(WorkoutService, 'listWorkouts').resolves({ results: [] })
 })
 
+// Required --serial flag to package.json so restoring would work
+// Instead of complaining about listWorkouts method being wrapped in all but 1 test
 test.afterEach.always(() => {
-  WorkoutService.listWorkouts.restore()
-});
+  sinon.restore()
+})
 
 test('workout api should return 200 w/ GET', async (t) => {
   const req = mockReq()
@@ -35,4 +41,16 @@ test('workout api should return 200 w/ GET', async (t) => {
   const returnsCorrectJSON = res.json.calledWith({ results: [] })
   t.true(responseStatus200)
   t.true(returnsCorrectJSON)
+})
+
+test('workout api should return 200 w/ POST with VALID data AND VALID user', async (t) => {
+  t.true(true)
+})
+
+test('workout api should return 400 w/ POST with INVALID data and VALID user', async (t) => {
+  t.true(true)
+})
+
+test('workout api should return 401 w/ POST with INVALID user', async (t) => {
+  t.true(true)
 })

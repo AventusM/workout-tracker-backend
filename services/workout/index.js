cfeadcfb2d524ded73cdcd89aba4cfb71ad77a27
Anton <anton.moroz@helsinki.fi>
2019-07-21 02:26:05 +0300

    Refactored workout services away from dependency-injection style. Putting a whole project on tutorial structure was a bad idea even though it handled mock testing (where traditional way seems to be working out any way so this style is not needed)

--- services/workout/index.js
+++ services/workout/index.js
@@ -1,5 +1,2 @@
-const Workout = require('../../models/workout')
-const Result = require('../../models/result')
 const WorkoutService = require('./workout_service')
-
-module.exports = WorkoutService(Workout, Result)
+module.exports = WorkoutService
--- services/workout/workout_service.js
+++ services/workout/workout_service.js
@@ -1,44 +1,37 @@
-const listWorkouts = Workout => () => {
-  // Populate with results BUT exclude user within a single result
-  // Populate user separately as workout is currently 1 person job
+const Workout = require('../../models/workout')
+const Result = require('../../models/result')
+
+const listWorkouts = () => {
   return Workout.find({}).populate('results')
 }
 
-// Attach user later on
-// Attach user later on
-// Attach user later on
-const createWorkout = (Workout, Result) => async (results, user_id) => {
+const createWorkout = async (results, user_id) => {
   if (!(results && user_id))
     throw new Error(`Results: ${results} -- User ID: ${user_id}`)
 
-  //1. Map results array into an array of Result objects
-  // save in a bulk https://stackoverflow.com/a/36090281
   const mappedResults = results.map(data => new Result({ data, user: user_id }))
+  // save in a bulk https://stackoverflow.com/a/36090281
   const createdResults = await Result.insertMany(mappedResults)
+  // map id's to be saved for workout session
   const savedResultsIds = createdResults.map(created => created._id)
 
-  //2. set resulted array to workout results
   const workoutSession = new Workout({ results: savedResultsIds, user: user_id })
-  //3. Save workout
   await workoutSession.save()
-  //4. Populate from result id's for POST data return
   return workoutSession.populate('results').execPopulate()
 }
 
-const deleteAllWorkouts = Workout => () => {
-  return Workout.deleteMany({})
-}
-
 // Also delete results associated with this workout?
-const deleteWorkoutById = Workout => (id) => {
+const deleteWorkoutById = (id) => {
   return Workout.findByIdAndRemove(id)
 }
 
-module.exports = (Workout, Result) => {
-  return {
-    listWorkouts: listWorkouts(Workout),
-    createWorkout: createWorkout(Workout, Result),
-    deleteAllWorkouts: deleteAllWorkouts(Workout),
-    deleteWorkoutById: deleteWorkoutById(Workout)
-  }
+const deleteAllWorkouts = () => {
+  return Workout.deleteMany({})
+}
+
+module.exports = {
+  listWorkouts,
+  createWorkout,
+  deleteAllWorkouts,
+  deleteWorkoutById
 }

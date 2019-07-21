097efcbf22325c78a301e6f546e49f3eb4052078
Anton <anton.moroz@helsinki.fi>
2019-07-21 03:21:21 +0300

    Base test for listing workouts done. Need to improve mock data model as it is inaccurate compared to the actual one

--- services/workout/__tests__/workout_service_list_workouts.test.js
+++ services/workout/__tests__/workout_service_list_workouts.test.js
@@ -3,14 +3,14 @@ const sinon = require('sinon').sandbox.create()
 const Workout = require('../../../models/workout')
 const WorkoutService = require('../index')
 
-const mockFindMethods = {
-  populate: function () {
-    return this
-  }
-}
+// Check listWorkouts method to see correct data model
+const originalData = [{ message: 'hi' }]
 
+// 1. SET FIND TO RETURN SOMETHING WITH SINON (need to figure out how to work with populate first...)
 test.beforeEach(() => {
-  sinon.stub(Workout, 'find').returns(mockFindMethods)
+  sinon.stub(Workout, 'find').returns({
+    populate: sinon.stub().returns(originalData)
+  })
 })
 
 test.afterEach.always(() => {
@@ -18,7 +18,10 @@ test.afterEach.always(() => {
 })
 
 test('listWorkouts should return a list of all workouts', async (t) => {
-  // 1. SET FIND TO RETURN SOMETHING WITH SINON (need to figure out how to work with populate first...)
   // 2. PERFORM WORKOUTS SEARCH
+  const foundWorkouts = await WorkoutService.listWorkouts()
+  // console.log('found workouts', foundWorkouts)
+  // console.log('mock workouts', originalData)
   // 3. COMPARE SEARCH (2) WITH ORIGINAL (1) -> IT SHOULD BE THE SAME
+  t.is(foundWorkouts, originalData)
 })
